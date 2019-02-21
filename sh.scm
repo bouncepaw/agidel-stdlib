32870e8a557aff10d2ff5be6188edfd4f922b881
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-21 22:46:12 +0500

    Update Agidel/sh

--- sh.scm
+++ sh.scm
@@ -1,21 +1,42 @@
 (module
  agidel-plugin.sh
- (set shift for-each-arg call echo)
+ (shebang! str set for-each-cli-arg _bracket)
  (import (prefix scheme -)
          (prefix chicken.base -)
          (prefix srfi-13 -)
          format)
 
- (-define (set var val)
-   (format "~A=~A\n" var val))
+ (-define (str lst)
+          (-if (-list? lst)
+               (-map (-lambda (x) (-if (-string? x)
+                                       (-string-append "\"" x "\"")
+                                       (-symbol->string x)))
+                     lst)
+               (-car (str (-list lst)))))
 
- (-define (shift) "shift")
+ (-define-syntax
+  shebang!
+  (syntax-rules ()
+    ((_) "#!/bin/sh\n")
+    ((_ path) (format "#!~A\n" path))))
 
- (-define (for-each-arg varname . exprs)
-          (format "for ~A in \"$@\"; do\n~A\ndone\n" (-string-join exprs "\n")))
+ (-define-syntax
+  set
+  (syntax-rules (+ -)
+    ((_ var val) (format "~A=~A\n" (str var) (str val)))
+    ((_ var + val) (format "~A+=~A\n" (str var) (str val)))
+    ((_ var - val) (format "~A-=~A\n" (str var) (str val)))))
 
- (-define (call . exprs)
-          (-string-join exprs " "))
-
- (-define (echo . exprs)
-          (-apply call "echo" exprs)))
+ (-define-syntax
+  for-each-cli-arg
+  (syntax-rules ()
+    ((_ var expr) (format "for ~A in \"$@\"; do\n~A\ndone\n"
+                             var
+                             expr))
+    ((_ var exprs ...) (format "for ~A in \"$@\"; do\n~A\ndone\n"
+                             var
+                             (-apply -string-append (-list exprs ...))))))
+ (-define-syntax
+  _bracket
+  (syntax-rules ()
+    ((_ exprs ...) (format "~A\n" (-string-join (str (-list exprs ...)) " "))))))
