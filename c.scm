e3da2690a3fcdd427ea9d52c6cda76db810cf338
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-22 20:10:02 +0500

    Redo Agidel/C. Add + - / % *

--- c.scm
+++ c.scm
@@ -1,133 +1,56 @@
-#| This one is crazy. All things from other modules are prefixed with -.
-|#
 (module
  agidel-plugin.c
- (_agidel-arities
-  /_bracket /_brace
-  /+ /- /* // /% ; arithmetical operators
-  /inc /inc* /dec /dec* ; -- ++
-  /or /and /not ; logic operators
-  /bitor /bitand /xor /compl /left-shift /right-shift ; bitwise operators
-  /eq? /neq? /< /> /<= /=> ; comparison operators
-  /import /#
-  /defun /defvar /defconst
-  )
- (import scheme
-         (chicken base)
-         (clojurian syntax)
-         (srfi 1)
-         (srfi 13)
-         format
-         (srfi 69))
+ *
+ (import (prefix scheme -)
+         (prefix chicken.base -)
+         (prefix chicken.string -)
+         (prefix srfi-13 -)
+         format)
 
- ;; By default, all the arguments get ævaled. You can override it here. Write q
- ;; to quote an arg, and e to æval it. Point represents rest-arg.
- (define _agidel-arities
-   (alist->hash-table
-    (map (lambda (pair) (cons (symbol-append '/ (car pair)) (cdr pair)))
-         '((_bracket . e) (_brace q)
-           (+ . e) (- . e) (* . e) (/ . e) (% . e)
-           (inc e) (inc* e) (dec e) (dec* e)
-           (or . e) (and . e) (not e)
-           (bitor . e) (bitand . e) (xor . e) (compl e)
-           (left-shift e e) (right-shift e e)
-           (eq? . e) (neq? . e) (< . e) (> . e) (<= . e) (=> . e)
-           (import . q) (# . q)
-           (defun q q . e) (defvar . q) (defvar . q)))))
+ (-define (str lst)
+          (-if (-list? lst)
+               (-map (-lambda (x) (-if (-string? x)
+                                       (-string-append "\"" x "\"")
+                                       (-->string x)))
+                     lst)
+               (-car (str (-list lst)))))
 
- ;; Brackets in Agidel/c are used to call C functions. Like that:
- ;; [printf "hello %s" "world"] → printf("hello %s", world);
- (define (/_bracket . os)
-   (define should-semicolon? #t)
-   (format "~A(~A)~A"
-           (car os)
-           (string-join (cdr os) ", " 'infix)
-           (if should-semicolon? ";\n" "")))
+ (-define (prefix->infix operator . operands)
+          (format "(~A)" (-string-join operands operator (-quote infix))))
 
- ;; Braces are used to insert literal text.
- ;; {foo bar} → foo bar
- (define (/_brace o)
-   (substring o 1 (- (string-length o) 1)))
- 
- ;; Unary function that just returns its operand.
- (define (as-is x) x)
+ (-define-syntax
+  +
+  (syntax-rules ()
+    ((_) (-error "Agidel: given no args to +"))
+    ((_ o) (format "+~A" o))
+    ((_ o ...) (-apply prefix->infix " + " (str (list o ...))))))
 
- ;; Function to make creation of operators easier.
- (define (prefix->infix-operator operands operator default-value one-operandλ)
-   (cond
-    [(null? operands) default-value]
-    [(= (length operands) 1) (apply one-operandλ operands)]
-    [else (format "(~A)" (string-join operands operator 'infix))]))
+ (-define-syntax
+  -
+  (syntax-rules ()
+    ((_) (-error "Agidel: given no args to -"))
+    ((_ o) (format "-(~A)" o))
+    ((_ o ...) (-apply prefix->infix " - " (str (list o ...))))))
 
- ;;; Arithmetical operators
- (define (/+ . os) (prefix->infix-operator os " + " "0" as-is))
- (define (/- . os)
-   (prefix->infix-operator os " - " "0"
-                           (lambda (o) (format "-(~A)" o))))
- (define (/* . os)
-   (prefix->infix-operator os " * " "1"
-                           (lambda (o) (format "*~A" o))))
- (define (// . os) (prefix->infix-operator os " / " "1" as-is))
- (define (/% . os) (prefix->infix-operator os " % " "1" as-is))
+ (-define-syntax
+  /
+  (syntax-rules ()
+    ((_) (-error "Agidel: given no args to /"))
+    ((_ o) (format "(1/~A)" o))
+    ((_ o ...) (-apply prefix->infix " / " (str (list o ...))))))
 
- ;;; Incrementors and decrementors
- (define (/inc o) (format "++(~A)" o))
- (define (/inc* o) (format "(~A)++" o))
- (define (/dec o) (format "--(~A)" o))
- (define (/dec* o) (format "(~A)--" o))
+ (-define-syntax
+  *
+  (syntax-rules ()
+    ((_) (-error "Agidel: given no args to *"))
+    ((_ o) (format "*~A" o))
+    ((_ o ...) (-apply prefix->infix " * " (str (list o ...))))))
 
- ;;; Logic operators
- (define (/or . os) (prefix->infix-operator os " || " "true" as-is))
- (define (/and . os) (prefix->infix-operator os " && " "false" as-is))
- (define (/not o) (format "!(~A)" o))
-
- ;;; Bitwise operators
- (define (/bitor . os) (prefix->infix-operator os " | " "1" as-is))
- (define (/bitand . os) (prefix->infix-operator os " & " "0" as-is))
- (define (/xor . os) (prefix->infix-operator os " ^ " "0" as-is))
- (define (/compl o) (format "~~(~A)" o))
- (define (/left-shift val shift) (format "(~A << ~A)" val shift))
- (define (/right-shift val shift) (format "(~A >> ~A)" val shift))
-
-
- (define (comparison-prefix->infix-operator os operator)
-   (cond
-    [(< (length os) 2)
-     (format #t "Operator ~A: arity error" operator)
-     (exit 1)]
-    [else
-     (let* ((curr-os (take os 2))
-            (rest-os (drop os 2))
-            (res (format "(~A ~A ~A)" (car curr-os) operator (cadr curr-os))))
-       (if (null? rest-os)
-           res
-           (/and res (comparison-prefix->infix-operator rest-os operator))))]))
- ;;; Comparison operators
- (define (/eq? . os) (comparison-prefix->infix-operator os " == "))
- (define (/neq? . os) (comparison-prefix->infix-operator os " != "))
- (define (/< . os) (comparison-prefix->infix-operator os " < "))
- (define (/> . os) (comparison-prefix->infix-operator os " > "))
- (define (/<= . os) (comparison-prefix->infix-operator os " <= "))
- (define (/=> . os) (comparison-prefix->infix-operator os " => "))
- 
-
- ;;; C preprocessor
- (define (/import . os)
-   (for-each (lambda (o)
-               (if (or (string-prefix? "<" o) (string-prefix? "\"" o))
-                   (format "#include ~A\n" o)
-                   (format "#include <~A>\n" o)))
-             os))
-
- (define (/# . os)
-   (format "#~A\n" (string-join os " " 'infix)))
-
-
- ;;; defsomethings
- (define (/defun name+type args . os))
-
- (define (/defvar . bindings))
-
- (define (/defconst . bindings))
+ (-define-syntax
+  %
+  (syntax-rules ()
+    ((_) (-error "Agidel: given no args to %"))
+    ((_ o) (-number->string (-exact->inexact (-/ o 100))))
+    ((_ o ...) (-apply prefix->infix " % " (str (list o ...))))))
 
  )
